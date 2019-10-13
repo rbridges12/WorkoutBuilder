7ea5c45578889dc43f02edc7eca49d9f83ce00ad
Riley Bridges <rlybrdgs@gmail.com>
2019-10-13 18:12:16 -0500

    Deleted private info

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from Workout import *
 
-test = Workout('english', 'test workout, 5x40/20s', 'Test Workout', 'minutes', 'percent')
-test.add_interval_set(5, 40/60, 150, 20/60, 20)
-test.export_mrp_file('C:/Users/rlybr/Desktop/Workouts')
+file_name = ''
+description = ''
+
+test = Workout(units = 'english', file_name = file_name, description = description, time_unit='minutes', ftp_unit='percent')
+
+test.add_interval(12, 30)
+test.add_interval_set(5, 3, 110, 1.5, 40)
+test.add_interval(10, 30)
+
+# Export workout as an MRP file to the specified destination
+test.export_mrp_file('')
